db2a9e5b66efc38d1307322f1499dbb445101a82
MicaelaLagana <lagana.mcl@gmail.com>
2020-08-30 14:24:37 -0300

    ejercicio uno

---
+++ ejercicio_uno/ejercicio_uno/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ejercicio_uno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("adivina el numero del 1 al 9");
+            int correctNumber = 7;
+            Console.WriteLine("ingresa un numero: ");
+            var input = Console.ReadLine();
+            int inputNumber = int.Parse(input);
+
+            while (correctNumber != inputNumber) {
+                Console.WriteLine("intenta de nuevo: ");
+                inputNumber = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("correcto");
+        }
+    }
+}
